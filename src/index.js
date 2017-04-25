1a68958002a360145d236499ae99a220bf1dd053
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-04-25 12:55:10 +0100

    work in progress: note controller

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function showInIndex() {
+  var noteList = new NoteList();
+  var noteController = new NoteController(noteList);
+  noteController.addNoteToIndex("Favourite drink: Seltzer");};
+
+  showInIndex();
--- src/note-controller.js
+++ src/note-controller.js
@@ -1,1 +1,12 @@
-document.getElementById('app').innerHTML = "Hola";
+(function(exports) {
+  function NoteController(noteList) {
+    this.noteListView = new NoteListView(noteList);
+  }
+
+  NoteController.prototype.addNoteToIndex = function(text) {
+    this.noteListView.noteList.createNote(text);
+    document.getElementById('app').innerHTML = this.noteListView.getHTML();
+  }
+
+exports.NoteController = NoteController;
+})(this);

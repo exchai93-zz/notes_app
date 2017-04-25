89cd0afde8f49b22f86e9ea47f9a5318a1273cfa
Emily Chai <echai93@gmail.com>
2017-04-25 11:30:11 +0100

    Add tests for viewing no notes and many notes

--- spec/note-list-viewSpec.js
+++ spec/note-list-viewSpec.js
@@ -8,3 +8,24 @@ function testNoteListView() {
 };
 
 testNoteListView();
+
+function testViewNoNotes() {
+  var noteList = new NoteList();
+  var view = new NoteListView(noteList);
+
+  assert.isTrue(view.getHTML().includes(""));
+}
+
+testViewNoNotes();
+
+function testViewManyNotes() {
+  var noteList = new NoteList();
+  noteList.createNote("Favourite club: Fabric");
+  noteList.createNote("Favourite sea animal: Jellyfish");
+  var view = new NoteListView(noteList);
+
+  assert.isTrue(view.getHTML().includes("<ul><li><div>Favourite club: Fabric</div></li></ul>"));
+  assert.isTrue(view.getHTML().includes("<ul><li><div>Favourite sea animal: Jellyfish</div></li></ul>"));
+}
+
+testViewManyNotes();
